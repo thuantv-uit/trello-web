a8457a8f5a57defea14494715ff7b564ca3fa0ef
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-29 15:45:14 +0700

    Thuan commit: Dong bo chuan Dark Mode & Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,63 @@ import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
 
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+// import useMediaQuery from '@mui/material/useMediaQuery'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light"
+        id="label-select-dark-light"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <div style={{ display: 'flex', alignContent: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize='small'></LightModeIcon>Ligth
+          </div>
+        </MenuItem>
+
+        <MenuItem value='dark'>
+          <div style={{ display: 'flex', alignContent: 'center', gap: '8px' }}>
+            <DarkModeOutlinedIcon fontSize='small'></DarkModeOutlinedIcon>Dark
+          </div>
+        </MenuItem>
+
+        <MenuItem value='system'>
+          <div style={{ display: 'flex', alignContent: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize='small'></SettingsBrightnessIcon>System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
+
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
+  // console.log('Dark: ', prefersDarkMode)
+  // console.log('Light', prefersLightMode)
+
   return (
     <Button
       onClick={() => {
@@ -22,6 +77,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect/>
+      <hr />
       <ModeToggle />
       <hr />
       <div>Hello Thuan</div>

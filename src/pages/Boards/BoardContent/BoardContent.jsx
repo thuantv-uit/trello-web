0d7256719925cb28dc1f56e170914d67fdd73eba
tvt04102004@gmail.com <tvt04102004@gmail.com>
2025-01-02 01:02:15 +0700

    Thuan commit: fix bug for part Drop and Drap

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,20 +1,57 @@
 import { Box } from '@mui/material'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+// import { DndContext } from '@dnd-kit/core'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
 
 function BoadrContent({ board }) {
+  // const pointerSensor = useSensor(PointerSensor, {activationConstraint: { distance: 10 } })
+  // yêu cầu chuột di chuyển 10px thì mới chuyển mới kích hợp event, fix trường hợp click bị gọi event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+  const mySensors = useSensors(mouseSensor, touchSensor)
 
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const [orderedColumns, setOrderedColumns] = useState( [] )
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+    // Kiểm tra nếu không tồn tại over (kéo ra nơi không có data và cick vào nó thì return để tránh lỗi)
+    if (!over) return
+    // Nếu vị trí sau khi kéo thả khác với vị trị ban đầu
+    if (active.id !== over.id ) {
+      // Lấy vị trí cũ (từ active)
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      // Lấy vị trí cũ (từ active)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // Cập nhật lại state columns bao đầu sau khi kéo thả
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
 
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode == 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={orderedColumns}/>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode == 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns}/>
+      </Box>
+    </DndContext>
   )
 }
 

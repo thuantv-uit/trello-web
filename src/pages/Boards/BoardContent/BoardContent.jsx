780c9cbd3474642a6ee01f44ee70eea85f23683b
tvt04102004@gmail.com <tvt04102004@gmail.com>
2025-01-01 23:18:05 +0700

    Thuan commit: Design Drap and Drop for columns

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -2,19 +2,71 @@ import { Box } from '@mui/material'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
 
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
+
 function BoadrContent({ board }) {
 
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // const pointerSensor = useSensor(PointerSensor, {activationConstraint: { distance: 10 } })
+  // yêu cầu chuột di chuyển 10px thì mới chuyển mới kích hợp event, fix trường hợp click bị gọi event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  // nhấn giữ 250ms và dung sai của cảm ứng 500 thì mới kích hoạt event
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
 
+  const mySensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board.columnOrderIds, board?._id))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    // console.log('handleDrapEnd: ', event)
+    const { active, over } = event
+
+    // Kiểm tra nếu không tồn tại over (kéo ra nơi không có data và cick vào nó thì return để tránh lỗi)
+    if (!over) return
+
+    // Nếu vị trí sau khi kéo thả khác với vị trị ban đầu
+    if (active.id !== over.id) {
+      // Lấy vị trí cũ (từ active)
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      // Lấy vị trí mới (từ over)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+
+      // Dùng arrayMove của dnd-kit để sắp xếp lại mảng Columns ban đầu
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // 2 cái cosole.log dữ liệu này sau dùng để gọi sữ lí gọi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndOrderedColumns: ', dndOrderedColumns)
+      // console.log('dndOrderedColumnsIds: ', dndOrderedColumnsIds)
+
+      // Cập nhật lại state columns bao đầu sau khi kéo thả
+      setOrderedColumns(dndOrderedColumns)
+    }
+
+  }
+
+  // sensor={mySensor}
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode == 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={orderedColumns}/>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} >
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode == 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,45 +2,46 @@ import { Box } from '@mui/material'
 import Column from './Column/Column'
 import { Button } from '@mui/material'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListColumns({ columns }) {
 
   return (
-    <Box sx={{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      '&::-webkit-scrollbar-track': { m: 2 }
-    }}>
-      {columns?.map(column => (<Column key={column._id} column={column} />))}
-      {/* syntax: () = return {} */}
-
-
-      {/* Button add  */}
+    <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
       <Box sx={{
-        minWidth: '200px',
-        maxWidth: '200px',
-        bgcolor: '#ffffff3d',
-        mx: 2,
-        borderRadius: '6px',
-        height: 'fit-content'
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        '&::-webkit-scrollbar-track': { m: 2 }
       }}>
-        <Button
-          startIcon= {<NoteAddIcon />}
-          sx={{
-            color: 'white',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 2.5,
-            py: 1
-          }}>
-          Add new column
-        </Button>
+        {/* syntax: () = return {} */}
+        {columns?.map(column => (<Column key={column._id} column={column} />))}
+        {/* Button add  */}
+        <Box sx={{
+          minWidth: '200px',
+          maxWidth: '200px',
+          bgcolor: '#ffffff3d',
+          mx: 2,
+          borderRadius: '6px',
+          height: 'fit-content'
+        }}>
+          <Button
+            startIcon= {<NoteAddIcon />}
+            sx={{
+              color: 'white',
+              width: '100%',
+              justifyContent: 'flex-start',
+              pl: 2.5,
+              py: 1
+            }}>
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 

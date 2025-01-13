43b5dfe0410915fbc5e9b352b30986cc19ca0cfd
tvt04102004@gmail.com <tvt04102004@gmail.com>
2025-01-13 16:14:51 +0700

    Thuan commit: fix 1st

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,15 +4,15 @@ import { Container } from '@mui/material'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoadrContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock_data'
-import { fetchBoardDatailsAPI } from '~/apis/index'
+// import { mockData } from '~/apis/mock_data'
+import { fetchBoardDatailsAPI } from '~/apis'
 
 function Board() {
 
   const [board, setBoard] = useState(null)
   useEffect(() => {
     // Hiện tại fix cứng boardId, về sau sẽ sử dụng react-router-dom để lấy chuẩn boardId từ URL về.
-    const boardId = '677f30ff91975e5c22652bdf'
+    const boardId = '6783ac4b67296cc4957c45fa'
     // Call API
     fetchBoardDatailsAPI(boardId).then(board => {
       setBoard(board)
@@ -23,8 +23,8 @@ function Board() {
     <>
       <Container disableGutters maxWidth={false} sx={{ height:'100vh' }}>
         <AppBar/>
-        <BoardBar board={mockData.board} />
-        <BoadrContent board={mockData.board} />
+        <BoardBar board={board} />
+        <BoadrContent board={board} />
       </Container>
     </>
   )
